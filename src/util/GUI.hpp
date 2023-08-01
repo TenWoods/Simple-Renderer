2fcde309abf291f74a003c5f21ecd977f606ed79
Wenxiang Yang <jukisan0525@gmail.com>
2023-08-01 20:06:16 +0900

    add SObject

---
+++ src/util/GUI.hpp
@@ -0,0 +1,12 @@
+#ifndef SIMPLE_RENDERER_GUI_HPP
+#define SIMPLE_RENDERER_GUI_HPP
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+namespace GUI
+{
+    //TODO
+}
+
+#endif //SIMPLE_RENDERER_GUI_HPP
