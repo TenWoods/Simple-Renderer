d4757bce99cdd4758e2870067e69cb36b59c74f3
tooki <jukisan0525@gmail.com>
2023-08-17 12:46:00 +0900

    add abstract light

--- src/objects/SLight.hpp
+++ src/objects/SLight.hpp
@@ -1,11 +1,19 @@
 #ifndef SIMPLE_RENDERER_SLIGHT_HPP
 #define SIMPLE_RENDERER_SLIGHT_HPP
+#include <glm/vec3.hpp>
 
 namespace SRenderer
 {
     class SLight
     {
-
+    private:
+        glm::vec3 position;
+        glm::vec3 color;
+    public:
+        void set_position(float x, float y, float z);
+        void set_position(glm::vec3 pos);
+        void set_color(float r, float g, float b);
+        void set_color(glm::vec3 col);
     };
 }
 
